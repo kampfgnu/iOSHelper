1af87fbee0542d91e7ef3eefa77043c72fe890ec
kampfgnu <feinahabl@gmx.at>
2013-07-11 14:38:52 +0200

    correct scale for unerasing imageview

--- iOSHelper/Classes/Subclasses/KGDrawEraseImageView.m
+++ iOSHelper/Classes/Subclasses/KGDrawEraseImageView.m
@@ -42,28 +42,62 @@
     _unerase = NO;
 }
 
+- (CGFloat)zoomScaleThatFits:(CGSize)target source:(CGSize)source {
+    CGFloat w_scale = (target.width / source.width);
+    CGFloat h_scale = (target.height / source.height);
+    
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    if (UIInterfaceOrientationIsPortrait(orientation)) {
+        return ((w_scale < h_scale) ? w_scale : h_scale);
+    }
+    else {
+        return w_scale;
+    }
+}
+
+- (CGPoint)scalePoint:(CGPoint)point scale:(CGFloat)scale {
+    return CGPointMake(point.x * scale, point.y * scale);
+}
+
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     
-    self.previousPoint1 = [touch previousLocationInView:self];
-    self.previousPoint2 = [touch previousLocationInView:self];
-    self.currentPoint = [touch locationInView:self];
+    if (_unerase && self.originalImage != nil) {
+        CGFloat scaleFactor = [self zoomScaleThatFits:self.originalImage.size source:self.frame.size];
+        self.previousPoint1 = [self scalePoint:[touch previousLocationInView:self] scale:scaleFactor];
+        self.previousPoint2 = self.previousPoint1;
+        self.currentPoint = [self scalePoint:[touch locationInView:self] scale:scaleFactor];
+    }
+    else {
+        self.previousPoint1 = [touch previousLocationInView:self];
+        self.previousPoint2 = self.previousPoint1;
+        self.currentPoint = [touch locationInView:self];
+    }
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     
-    self.previousPoint2 = self.previousPoint1;
-    self.previousPoint1 = [touch previousLocationInView:self];
-    self.currentPoint = [touch locationInView:self];
+    if (_unerase && self.originalImage != nil) {
+        CGFloat scaleFactor = [self zoomScaleThatFits:self.originalImage.size source:self.frame.size];
+        self.previousPoint2 = self.previousPoint1;
+        self.previousPoint1 = [self scalePoint:[touch previousLocationInView:self] scale:scaleFactor];
+        self.currentPoint = [self scalePoint:[touch locationInView:self] scale:scaleFactor];
+    }
+    else {
+        self.previousPoint2 = self.previousPoint1;
+        self.previousPoint1 = [touch previousLocationInView:self];
+        self.currentPoint = [touch locationInView:self];
+    }
     
     // calculate mid point
     CGPoint mid1 = KGMidPointBetweenCGPoints(self.previousPoint1, self.previousPoint2);
     CGPoint mid2 = KGMidPointBetweenCGPoints(self.currentPoint, self.previousPoint1);
     
-    UIGraphicsBeginImageContext(self.frame.size);
+    CGSize originalImageSize = self.originalImage.size;
+    UIGraphicsBeginImageContext(originalImageSize);
     CGContextRef context = UIGraphicsGetCurrentContext();
-    [self.image drawInRect:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+    [self.image drawInRect:CGRectMake(0, 0, originalImageSize.width, originalImageSize.height)];
     
     CGContextSetBlendMode(context, _unerase ? kCGBlendModeNormal : kCGBlendModeClear);
     
@@ -74,7 +108,7 @@
     CGContextSetLineCap(context, kCGLineCapRound);
     CGContextSetLineWidth(context, _lineWidth);
     
-    if (_unerase) [[UIColor colorWithPatternImage:self.originalImage] setStroke];
+    if (_unerase && self.originalImage != nil) [[UIColor colorWithPatternImage:self.originalImage] setStroke];
     else [[UIColor clearColor] setStroke];
     
     CGContextStrokePath(context);
