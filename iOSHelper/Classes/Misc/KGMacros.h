a89353b7cbca425f12b7c376d6a1d62e06e60cf1
kampfgnu <feinahabl@gmx.at>
2014-02-13 10:35:01 +0100

    added localization shortcut to macros

--- iOSHelper/Classes/Misc/KGMacros.h
+++ iOSHelper/Classes/Misc/KGMacros.h
@@ -11,3 +11,5 @@
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+#define _(key)	NSLocalizedString(key, key)
